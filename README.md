fc1206dde0218d4a1887f6741d00f1359f1c9181
Oscar Ko <OscarKoEmail@gmail.com>
2023-03-02 15:38:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://oscarkodes.github.io/d3_map_practice
---
+++ main.js
@@ -0,0 +1,27 @@
+/* CONSTANTS AND GLOBALS */
+const width = window.innerWidth *.8 ;
+const height = 500;
+
+const margin = {
+    right: 50,
+    left: 50,
+    top: 50,
+    bottom: 50
+}
+
+// /* LOAD DATA */
+// d3.csv('data.csv', d => {
+//   // use custom initializer to reformat the data the way we want it
+//   // ref: https://github.com/d3/d3-fetch#dsv
+//   return {
+//     date: new Date(d.dateTemperature),
+//     location: d.location,
+//     temperature: +d.temperature
+//   }
+// }).then(data => {
+
+
+ 
+// });
+
+console.log("Hello")
